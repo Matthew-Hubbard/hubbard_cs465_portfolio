31723999c206db4bdcaa00c3d556c9b67bc9d727
Matthew Hubbard <mrh8@pdx.edu>
2019-05-18 13:40:00 -0700

    Created a new file to put javascript. Had some trouble updating the navbar items active states and making the mouse cursor turn into a hand on hover but got it working. Now when you click on "About", it updates the main window.

---
+++ site.js
@@ -0,0 +1,9 @@
+
+function about() {
+    //alert("You clicked About!");
+    $("#Home").removeClass("active");
+    $("#Projects").removeClass("active");
+    $("#About").addClass("active");
+    $("#main_window").html("<h1>About</h1>");
+}
+
