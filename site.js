964e4c6d477be3646e95f50130d35a408bb8f141
Matthew Hubbard <mrh8@pdx.edu>
2019-05-18 14:11:48 -0700

    Created function to set the active navbar element. Got the navbar working for all three elements to update the main window div.

--- site.js
+++ site.js
@@ -1,9 +1,37 @@
 
-function about() {
-    //alert("You clicked About!");
+function setNav(navButton){
     $("#Home").removeClass("active");
     $("#Projects").removeClass("active");
-    $("#About").addClass("active");
-    $("#main_window").html("<h1>About</h1>");
+    $("#About").removeClass("active");
+    if(navButton === "Home") {
+        $("#Home").addClass("active");
+    }
+    else if(navButton === "Projects") {
+        $("#Projects").addClass("active");
+    }
+    else if(navButton === "About"){
+        $("#About").addClass("active");
+    }
+};
+
+function about() {
+    //alert("You clicked About!");
+    // $("#Home").removeClass("active");
+    // $("#Projects").removeClass("active");
+    // $("#About").addClass("active");
+    setNav("About");
+    let about_str = "I'm a computer science student at PSU planning to graduate in December of 2019.";
+    $("#main_window").html("<h1>About</h1><p>" + about_str + "</p>");
 }
 
+function projects() {
+    setNav("Projects");
+    let about_str = "projects here";
+    $("#main_window").html("<h1>Projects</h1><p>" + about_str + "</p>");
+}
+
+function home() {
+    setNav("Home");
+    $("#main_window").html("<h1>Matthew Hubbard</h1>" +
+        "<img src='email_profile_pic.jpg' alt='Matthew R. Hubbard' style='width:40%;height:40%;'>");
+}
