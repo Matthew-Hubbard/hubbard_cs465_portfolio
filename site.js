371ebe595667aeb791522a9ca56083eb92846e05
Matthew Hubbard <mrh8@pdx.edu>
2019-05-19 13:51:00 -0700

    Fleshed out my css. Chose a background image. Used fonts through google fonts. Changed the well links background and font. Changed the Projects header to a marker font. Linked resume.

--- site.js
+++ site.js
@@ -21,16 +21,16 @@ function about() {
     // $("#About").addClass("active");
     setNav("About");
     let about_str = "I'm a computer science student at PSU planning to graduate in December of 2019.";
-    $("#main_window").html("<h1>About</h1><p>" + about_str + "</p>");
+    $("#mainWindow").html("<h1>About</h1><p>" + about_str + "</p>");
 }
 
 function projects() {
     setNav("Projects");
     let about_str = "projects here";
-    $("#main_window").html("<h1>Projects</h1><p>" + about_str + "</p>");
+    $("#mainWindow").html("<h1 id='marker'>Projects</h1><p>" + about_str + "</p>");
 }
 
 function home() {
     setNav("Home");
-    $("#main_window").html("<h1>Matthew Hubbard's Portfolio</h1><p>Welcome to my portfolio site. Check out my about page and what projects I've done.</p>");
+    $("#mainWindow").html("<h1>Matthew Hubbard's Portfolio</h1><p>Welcome to my portfolio site. Check out my about page and what projects I've done.</p>");
 }
