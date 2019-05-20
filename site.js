b9ca89fb7a26720aeb1fd620b2e72212feb1ca3d
Matthew Hubbard <mrh8@pdx.edu>
2019-05-19 19:25:25 -0700

    Added to projects. Took awhile to figure out how to get the scroll bar working how I wanted and at the right height.

--- site.js
+++ site.js
@@ -26,8 +26,28 @@ function about() {
 
 function projects() {
     setNav("Projects");
-    let about_str = "projects here";
-    let htmlStr = "<h1 id='marker'>Projects</h1><p>" + about_str + "</p>";
+    let htmlStr = "<h1 id='marker'>Projects</h1>";
+    // Robot Project 1
+    htmlStr += "<h1 style='font-size: 28pt'>Arduino Robot</h1>";
+    htmlStr += "<iframe width='640' height='360' src='https://www.youtube.com/embed/rlwySEcKMw0?rel=0?version=3&autoplay=1&controls=1&&showinfo=0&loop=1​' " +
+        "frameborder='1' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+    htmlStr += "<p style='font-size: 20pt; text-align: left;'>I built a small robot using an Arduino Uno microprocessor, L293d IC motor driver, and a small dc motor.</p>";
+
+    //This website Project 2
+    htmlStr += "<hr style='border-color:black;'>";
+    htmlStr += "<h1 style='font-size:28pt'>Portfolio Website</h1>";
+    htmlStr += "<div style='font-size:20pt;text-align:left;'><p style='font-size:20pt;text-align:left;display:inline;'>" +
+        "I built this portfolio website for my web dev class. You can find the source" +
+        "code </p><a style='font-size:20pt;text-align:left;display:inline;' href='https://github.com/Matthew-Hubbard/hubbard_cs465_portfolio'>here</a>" +
+        "<p style='font-size:20pt;text-align:left;display:inline'>." +
+        " Bootstrap was used in conjunction with html, css, javascript, jquery.</p></div>";
+
+    //Capstone Project 3
+    htmlStr += "<hr style='border-color:black;'>";
+    htmlStr += "<h1 style='font-size:28pt'>Capstone: OIT Web Apps</h1>";
+    htmlStr += "<p style='font-size: 20pt; text-align: left; display:inline'>I'm team lead for my Capstone project at PSU. We're working with OIT to port their old grails applications" +
+        " to a more modern framework, Vue.js. The front-end code is available </p><a style='font-size:20pt;text-align:left;' " +
+        "href='https://github.com/psu-oit-capstone/emp-front-end'>here</a><p style='font-size: 20pt; text-align: left; display: inline'>.</p>";
     $("#mainWindow").html(htmlStr);
 }
 
